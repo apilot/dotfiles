03b041efb1619d1aca3bf7e47675a859c62714ae
Alex Boyarinov <a.boyarinov@gmail.com>
2024-08-08 20:57:52 +0400

    add lazyVim config

--- config/nvim.lazyvim/lua/config/options.lua
+++ config/nvim.lazyvim/lua/config/options.lua
@@ -1,16 +1,36 @@
 vim.cmd("let g:netrw_liststyle = 3")
+vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_floating_shadow = true
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 45
+vim.g.neovide_light_radius = 5
 
--- local opt = vim.opt -- for conciseness
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
+local opt = vim.opt -- for conciseness
 --
 -- -- line numbers
 -- opt.relativenumber = true -- show relative line numbers
 -- opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 --
 -- -- tabs & indentation
--- opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
--- opt.shiftwidth = 2 -- 2 spaces for indent width
--- opt.expandtab = true -- expand tab to spaces
--- opt.autoindent = true -- copy indent from current line when starting new one
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
 --
 -- -- line wrapping
 -- opt.wrap = false -- disable line wrapping
@@ -43,3 +63,8 @@ vim.cmd("let g:netrw_liststyle = 3")
 --
 -- -- turn off swapfile
 -- opt.swapfile = false
+opt.wrapmargin = 8 -- wrap lines when coming within n characters from side
+opt.linebreak = true -- set soft wrapping
+opt.showbreak = "↪ "
+vim.g.lazyvim_ruby_lsp = "solargraph"
+vim.g.lazyvim_ruby_formatter = "standardrb"
