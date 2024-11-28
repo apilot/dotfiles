aaf57c8413b1c6d6e24107449702d20e9dffe8c2
Alexey Boyarinov <a.boyarinov@gmail.com>
2024-11-28 16:57:00 +0400

    changes by 28.10.2024

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local keymap = vim.keymap.set
-keymap("n", "<leader>cz", "<cmd>ZenMode<cr>", { desc = "ZenMode toggle" }) -- split window vertically
-keymap("n", "<leader>ct", "<cmd>Twilight<cr>", { desc = "Twilight toggle" }) -- split window vertically
+keymap("n", "<leader>cz", "<cmd>ZenMode<cr>", { desc = "ZenMode toggle" })
+keymap("n", "<leader>ct", "<cmd>Twilight<cr>", { desc = "Twilight toggle" })
+keymap("n", "<leader>ce", "<c-e>", { desc = "Special edit opts", remap = true })
+keymap("n", "<leader>ceW", "<cmd>:e ++enc=cp1251<cr>", { desc = "CP1251 codepage" }) -- switch to cp1251 codepage
+keymap("n", "<leader>ceU", "<cmd>:e ++enc=utf-8<cr>", { desc = "UTF8 codepage" }) -- switch to cp1251 codepage
+keymap("n", "<leader>cec", "<cmd>:CsvViewToggle delimiter=, comment=#<cr>", { desc = "Edit CSV with , delimiter" }) -- switch CSV view with comma delimiter
+keymap("n", "<leader>ceC", "<cmd>:CsvViewToggle delimiter=; comment=#<cr>", { desc = "Edit CSV with ; delimiter" }) -- switch CSV view with ; delimiter
 keymap("n", "<leader>we", "<C-w>=", { desc = "Make windows equal size" }) -- make split windows equal width & height
 keymap(
   "n",
--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -1,4 +1,10 @@
 return {
   { "mg979/vim-visual-multi" },
   { "ThePrimeagen/git-worktree.nvim" },
+  {
+    "hat0uma/csvview.nvim",
+    config = function()
+      require("csvview").setup()
+    end,
+  },
 }
