498ce305ade18bbfe953340f552dde701d61d9e4
Alexey Boyarinov <a.boyarinov@gmail.com>
2025-06-21 01:01:30 +0400

    changes 20.06.2025
    change waybar theme

---
+++ hyprlands/.config/waybar/scripts/power-menu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+config="$HOME/.config/rofi/power-menu.rasi"
+
+actions=$(echo -e "  Lock\n  Shutdown\n  Reboot\n  Suspend\n  Hibernate\n󰞘  Logout")
+
+# Display logout menu
+selected_option=$(echo -e "$actions" | rofi -dmenu -i -config "${config}" || pkill -x rofi)
+
+# Perform actions based on the selected option
+case "$selected_option" in
+*Lock)
+	loginctl lock-session
+	;;
+*Shutdown)
+	loginctl poweroff
+	;;
+*Reboot)
+	loginctl reboot
+	;;
+*Suspend)
+	loginctl suspend
+	;;
+*Hibernate)
+	loginctl hibernate
+	;;
+*Logout)
+	loginctl kill-session "$XDG_SESSION_ID"
+	;;
+esac
